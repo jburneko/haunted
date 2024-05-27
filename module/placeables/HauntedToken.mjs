14a1e7fef057cfeeb1a63fc7979d62c3b5e30698
jburneko <jburneko@yahoo.com>
2024-05-26 21:36:58 -0700

    Fix Texture Unload

--- module/placeables/HauntedToken.mjs
+++ module/placeables/HauntedToken.mjs
@@ -1,26 +1,20 @@
 import { ActorToSVG } from "../utlis/actor-to-svg.mjs";
 
 export class HauntedToken extends Token {
-
-    static redrawToken(actorId) {
-        for (const token of canvas.tokens.placeables) {
-            if(token.document.actorId === actorId) {
-                token.redraw();
-            }
-        }
+  static redrawToken(actorId) {
+    for (const token of canvas.tokens.placeables) {
+      if (token.document.actorId === actorId) {
+        token.redraw();
+      }
     }
+  }
 
-    clearCache() {
-        const oldTexture = TextureLoader.loader.getCache(this.document.texture.src);
-        const srcURL = oldTexture?.resource?.source?.src;
-        if (srcURL) URL.revokeObjectURL(srcURL);
-        if (!oldTexture?._destroyed) oldTexture?.destroy(true);
-    }
-
-    async redraw() {
-        this.clearCache();
-        const newTexture = await loadTexture(this.document.texture.src);
-        this.texture = newTexture
-        this.mesh.texture = newTexture
-    } 
+  async redraw() {
+    const src = this.document.texture.src;
+    await PIXI.Assets.unload(src);
+    const newTexture = await PIXI.Assets.load(src);
+    TextureLoader.loader.setCache(src, newTexture);
+    this.texture = newTexture;
+    this.mesh.texture = newTexture;
+  }
 }
